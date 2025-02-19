602694d2290c0a926f07ad783683210d7eadec4c
Nicolas Ajurias <ajuriasn@shernet.sheridancollege.ca>
2025-02-19 11:24:53 -0500

    added player movement script.

--- Assets/Scripts/DeadlyPlayer.cs
+++ Assets/Scripts/DeadlyPlayer.cs
@@ -6,13 +6,14 @@ public class DeadlyPlayer : MonoBehaviour
 {
 
     public float speed = 3;
-    public float gravity = 1;
-    public float jumpSpeed = 5;
+    public float jumpSpeed = 8;
+    public bool isJumping;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        isJumping = false;
+        jumpSpeed = 8;
     }
 
     // Update is called once per frame
@@ -21,6 +22,26 @@ public class DeadlyPlayer : MonoBehaviour
         Vector2 pos = transform.position;
 
         pos.x += Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+        
+
+        if (Input.GetKeyDown(KeyCode.Space) && isJumping == false)
+        {
+            isJumping = true;
+        }
+        if (isJumping == true)
+        {
+            pos.y += jumpSpeed * Time.deltaTime;
+            jumpSpeed -= 3.5f * Time.deltaTime;
+        }
+        if (jumpSpeed <= 0)
+        {
+            isJumping = false;
+        }
+        if(isJumping == false)
+        {
+            jumpSpeed = 8;
+        }
 
         transform.position = pos;
     }
